0f5f23fb3af6a4d1b535fe9a2bf21457b07dba1f
sinankocatrk <sinankocaturk0@gmail.com>
2022-03-11 00:28:26 +0300

    c

---
+++ yasbulma.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+
+int yil,yas;
+printf("dogum yili giriniz:");
+scanf("%d",&yil);
+
+
+
+if(yil>2022){
+    printf("Henuz dogmadiniz =) ");
+
+
+
+}
+else{
+    yas=2022-yil;
+printf("yasiniz: %d",yas);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
